4fd842f8c58d5aa52133d47a772a4b5d6c2998ee
umiSirya <161163423+umiSirya@users.noreply.github.com>
2024-06-24 21:37:51 +0300

    added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function updateWeather(response) {
+  let temperature = document.querySelector("#temp");
+  let cityElement = document.querySelector("#city");
+
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "b0e75af401t952a8ab0ba812a00o3fdc";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchsubmitted(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchsubmitted);
+searchCity("Nairobi");
