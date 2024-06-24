c9f8072f30d44ca74414913f109ac87ef713a05a
umiSirya <161163423+umiSirya@users.noreply.github.com>
2024-06-24 23:24:19 +0300

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 function updateWeather(response) {
   let temperature = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#weather-description");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let time = document.querySelector("#time");
 
+  // Create a date object from the response time
+  let date = new Date(response.data.time * 1000);
+
+  // Update the UI elements with the data from the response
   temperature.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  description.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed} km/h`;
+
+  // Pass the created date object to the formatDate function
+  time.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes(); // Use the date object passed as an argument
+  let hours = date.getHours();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  // Ensure two digits for minutes
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
